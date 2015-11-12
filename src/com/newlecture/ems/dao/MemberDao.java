a94b1e3839a5c3621e62c9e8d2cdcf14e30d7992
newlec <newlec123@gmail.com>
2015-11-12 12:40:45 +0900

    nelwec : 프로젝트 초기화

---
+++ src/com/newlecture/ems/dao/MemberDao.java
@@ -0,0 +1,48 @@
+package com.newlecture.ems.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.newlecture.ems.vo.Member;
+
+public class MemberDao {
+	
+	public List<Member> getMembers() throws SQLException{
+		return getMembers("");
+	}
+	
+	public List<Member> getMembers(String query) throws SQLException{
+		
+		String sql = "SELECT * FROM MEMBERS WHERE NAME LIKE '%"+query+"%'";
+		
+		String url = "jdbc:oracle:thin:@211.238.142.251:1521:orcl";			
+		Connection con = DriverManager.getConnection(url, "c##sist", "dclass");
+		
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(sql);
+		
+		List<Member> list = new ArrayList<Member>();
+		Member member = null;
+		
+		while(rs.next())	{
+			member = new Member();
+			
+			member.setMid(rs.getString("mid"));
+			member.setName(rs.getString("name"));
+			member.setPwd(rs.getString("age"));
+			
+			list.add(member);
+		}		
+								
+		rs.close();
+		st.close();
+		con.close();
+		
+		return list;
+	}
+}
---
+++ src/com/newlecture/ems/vo/Member.java
@@ -0,0 +1,29 @@
+package com.newlecture.ems.vo;
+
+public class Member {
+	private String mid;
+	private String name;
+	private String pwd;
+	
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
+	
+	
+}
